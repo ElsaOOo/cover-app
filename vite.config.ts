095c9ecc8b21eb060e65079530367fa99d25a1a3
ElsaOOo <yjjjng0901@126.com>
2021-08-28 18:39:30 +0800

    fix: 打包问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import styleImport from 'vite-plugin-style-import'
 const path = require('path')
 
 // https://vitejs.dev/config/
@@ -10,22 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [
-    vue(),
-    styleImport({
-      libs: [
-        {
-          libraryName: 'element-plus',
-          esModule: true,
-          ensureStyleFile: true,
-          resolveStyle: (name) => {
-            return `element-plus/lib/theme-chalk/${name}.css`
-          },
-          resolveComponent: (name) => {
-            return `element-plus/lib/${name}`
-          },
-        },
-      ],
-    }),
-  ],
+  plugins: [vue()],
 })
